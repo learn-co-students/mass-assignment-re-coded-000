118e10a56252ca879ae0ce0935a8e3007bfa7e27
dina aziz <dina.aziz100@yahoo.com>
2017-02-19 04:59:22 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,8 @@ class Person
   :height, :weight, :handed, :complexion, :t_shirt_size,
   :wrist_size, :glove_size, :pant_length, :pant_width
   def initialize(attributes)
-      attributes.each do |k,v|
-        self.send("#{k}=", v)
+      attributes.each do |key,value|
+        self.send("#{key}=", value)
       end
     end
 end
